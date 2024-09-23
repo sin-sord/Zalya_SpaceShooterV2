ed81ba1b5bbd12c08cdad9a828eaecda20b30483
sin-sord <kevkinzz@gmail.com>
2024-09-23 12:04:27 -0400

    Updated player.SC
    Added a directional movement.

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Codice.CM.Common.Tree;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,9 +10,43 @@ public class Player : MonoBehaviour
     public GameObject bombPrefab;
     public Transform bombsTransform;
 
+    //public Vector3 velocity = new Vector3(0.001f, 0);
+
     void Update()
     {
+      //  transform.position += velocity;
+       
+        
+         PlayerMovement();
+    }
 
+    void PlayerMovement()
+    {
+        Vector3 moveX = new Vector3(0.01f, 0);  //  specifies the movement of the x-axis
+        Vector3 moveY = new Vector3(0, 0.01f);  // specifies the movement of the y-axis
+       
+
+        if (Input.GetKey(KeyCode.LeftArrow))  //  moves the player to the left
+        {
+            transform.position -= moveX;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))  //  moves the player to the right
+        {
+            transform.position += moveX;
+        }
+
+
+        if (Input.GetKey(KeyCode.UpArrow))  //  moves the player up
+        {
+            transform.position += moveY;
+        }
+
+
+        if (Input.GetKey(KeyCode.DownArrow))  //  moves the player down
+        {
+            transform.position -= moveY;
+        }
     }
 
 }
