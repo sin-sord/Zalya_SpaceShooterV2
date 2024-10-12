bcb656a1e06fe9efdad42134d4f603f6d6f1e59b
sin-sord <kevkinzz@gmail.com>
2024-10-12 15:12:51 -0400

    Week 4 - Task 3 complete
    Made a moon rotate around a planet

--- Assets/Scripts/Controllers/Moon.cs
+++ Assets/Scripts/Controllers/Moon.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class Moon : MonoBehaviour
 {
     public Transform planetTransform;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float direction;
 
     // Update is called once per frame
     void Update()
     {
+        OrbitalMotion(4, direction, planetTransform);
+    }
 
+    public void OrbitalMotion(float radius, float speed, Transform target)
+    {
+        transform.RotateAround(target.position, Vector3.forward, speed * Time.deltaTime);
     }
 }
